ae94c2933333f18f948b8810fd28c9a09bc2c35d
lucas-heewook-park <lucas.heewook.park@gmail.com>
2018-11-13 17:04:03 +0900

    05websocket

--- 08nodejs-websocket-server_edu/ws.js
+++ 08nodejs-websocket-server_edu/ws.js
@@ -1,1 +1,21 @@
-//Simple Server
+//Simple Server WebSocket 작성
+const WebSocket = require('ws');
+//WebSocket Server 객체 생성
+const wss = new WebSocket.Server({port: 40510});
+
+//connection, message event 처리
+wss.on('connection', function (ws) {
+    //client에서 보낸 메시지를 server에서 수신 받음
+    ws.on('message', function (msg) {
+        console.log('수신 받은 메시지: %s', msg);
+    });
+    //server에서 client에게 메시지를 전송
+    // setInterval(function (value) {
+    //     return value + 1;
+    // }, 3000);
+    setInterval(() => ws.send(`${new Date()}`),
+        3000);
+});
+
+
+
